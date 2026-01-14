9b14bf8c4ac3cab869c53639b43bd13b04abf33c
Muhammad Ibrahim <nas.61975@yahoo.com>
2026-01-14 21:40:21 +0530

    Enhance AboutUs component with detailed content
    Updated the AboutUs component to provide detailed information about Paradise Nursery.

--- src/components/AboutUs.js
+++ src/components/AboutUs.js
@@ -1,5 +1,17 @@
 function AboutUs() {
-  return <div>About Us Page</div>;
+  return (
+    <div className="about-us-container">
+      <h1>About Paradise Nursery</h1>
+      <p>
+        Paradise Nursery is an online plant shopping platform dedicated to
+        providing healthy, beautiful indoor and outdoor plants.
+      </p>
+      <p>
+        Our mission is to bring nature closer to your home by offering
+        affordable plants with an easy shopping experience.
+      </p>
+    </div>
+  );
 }
 
 export default AboutUs;
